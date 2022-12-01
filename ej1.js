6529de4bca92d04b48965dd8339bf0277ef82791
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2022-12-01 18:30:14 +0000

    ejercicio 1

---
+++ ej1.js
@@ -0,0 +1,34 @@
+/*
+This year the elves have bought a gift wrapping machine. But... it's not programmed! We need to create an algorithm that helps it in the task.
+The machine receives an array with the gifts. Each gift is a string. We need the machine to wrap each gift in wrapping paper and place it in an array of wrapped gifts.
+The wrapping paper is the * symbol and to wrap a gift the * symbol is placed so that it completely surrounds the string on all sides. For example:
+const gifts = ['book', 'game', 'socks']
+const wrapped = wrapping(gifts)
+console.log(wrapped)
+```
+[
+  "******\n*book*\n******",
+  "******\n*game*\n******",
+  "*******\n*socks*\n*******"
+]
+```
+As you can see, the wrapping paper wraps the string. On top and bottom, so as not to leave any gaps, the corners are also covered with wrapping paper.
+Good luck!
+*/
+
+function wrapping(gifts) {
+  let len, star;
+  let regularExpression = /^\*/;
+  gifts.forEach((gift, index) => {
+    if (!gift.match(regularExpression)) {
+      len = gift.length + 2;
+      star = '*'.repeat(len);
+      gifts[index] = `${star}\n*${gift}*\n${star}`;
+    }
+  });
+  return gifts;
+}
+
+const gifts = ['******\n*book*\n******', 'game', 'socks']
+const wrapped = wrapping(gifts)
+console.log(wrapped)
