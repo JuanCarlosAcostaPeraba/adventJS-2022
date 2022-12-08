b20e42f33e9c2815b3481715b18d56a8ed6cacf6
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2022-12-08 13:16:03 +0000

    ejercicio 7

---
+++ ej7.js
@@ -0,0 +1,55 @@
+/*
+En los almacenes de Papá Noel están haciendo inventario. Hay tres almacenes (que se representa cada uno como un Array). En cada almacén hay regalos.
+Nos han pedido que escribamos un programa que nos diga qué regalos hay que comprar para reponer en nuestros almacenes ahora que se acerca la Navidad. Un regalo se tiene que reponer cuando sólo hay stock en uno de los tres almacenes.
+Por ejemplo, si tenemos los siguientes almacenes:
+*/
+
+const a1 = ['bici', 'coche', 'bici', 'bici']
+const a2 = ['coche', 'bici', 'muñeca', 'coche']
+const a3 = ['bici', 'pc', 'pc']
+
+/* El almacén a1 tiene "bici" y "coche".
+El almacén a2 tiene "coche", "bici" y "muñeca".
+El almacén a3 tiene "bici" y "pc".
+
+El regalo "muñeca" y "pc" sólo están en los almacenes a2 y a3 respectivamente.
+*/
+
+//const gifts = getGiftsToRefill(a1, a2, a3) // ['muñeca', 'pc']
+const gifts = getGiftsToRefill(['a', 'b'], ['c', 'd'], ['e', 'f']) // ['muñeca', 'pc']
+
+/*
+Como ves, los almacenes pueden tener el mismo regalo repetido varias veces. Pero, por más existencias que haya en un almacén, si no tenemos en los otros dos, debemos reponerlo para tener mejor distribución.
+📝 Summary
+Crea una función getGiftsToRefill que reciba tres Array como parámetros.
+La función debe devolver un Array con los regalos que hay que reponer.
+Un regalo se debe reponer cuando sólo hay stock en uno de los tres almacenes.
+Si no hay ningún regalo que reponer, la función debe devolver un Array vacío.
+Si hay más de un regalo que reponer, la función debe devolver un Array con todos los regalos que hay que reponer.
+*/
+
+function getGiftsToRefill(a1, a2, a3) {
+  let result = [];
+
+  for (let i = 0; i < a1.length; i++) {
+    if (!a2.includes(a1[i]) && !a3.includes(a1[i]) && !result.includes(a1[i])) {
+      result.push(a1[i]);
+    }
+  }
+
+  for (let i = 0; i < a2.length; i++) {
+    if (!a1.includes(a2[i]) && !a3.includes(a2[i]) && !result.includes(a2[i])) {
+      result.push(a2[i]);
+    }
+  }
+
+  for (let i = 0; i < a3.length; i++) {
+    if (!a1.includes(a3[i]) && !a2.includes(a3[i]) && !result.includes(a3[i])) {
+      result.push(a3[i]);
+    }
+  }
+
+  return result;
+}
+
+console.log(gifts);
